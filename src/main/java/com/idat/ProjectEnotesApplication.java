1b9ae84e7b320f9958dfb4b00f58ba41299e483b
Paul <paullopez8578@gmail.com>
2024-12-15 11:32:45 -0500

    initial commit

---
+++ src/main/java/com/idat/ProjectEnotesApplication.java
@@ -0,0 +1,13 @@
+package com.idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEnotesApplication.class, args);
+	}
+
+}
